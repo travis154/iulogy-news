9058b5889273a9cf09d1cc18bca2eb6ae04cb19a
iuthisaam <iuthisaam@isa0004.(none)>
2012-12-05 09:04:26 +0500

    changed from client rendering to server rendering

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ var express = require('express')
   , http = require('http')
   , path = require('path')
   , jade_browser = require('jade-browser')
+  , redis = require('redis')
+  , async = require('async')
+var client = redis.createClient();
 
 var app = express();
 
@@ -30,9 +33,93 @@ app.configure('development', function(){
   app.use(express.errorHandler());
 });
 
-app.get('/', routes.index);
-app.get('/users', user.list);
+app.get('/', function(req, res){
+	getData(req, function(data){
+		var column = 1;
+		var column1 = [];
+		var column2 = [];
+		for(var source in data.content){
+			var src = data.content[source];
+			if(src.length == 0) continue;
+			//set first element as the first object to hold an image
+			var main;
+			for(var i = 0; i < data.content[source].length; i++){
+				if(typeof data.content[source][i].images == 'object'){
+					main = src.splice(i,1)[0];
+					break;
+				}
+			}
+			//set large image for main image
+			if(typeof main.images == 'object'){
+				for(var i = 0; i < main.images.sizes.length; i++){
+					if( main.images.sizes[i].width <= 480 && main.images.sizes[i].width > 320){
+						main.images.thumb = main.images.sizes[i].source;
+						break;
+					}
+				}
+				if(main.images.thumb == ""){
+					main.images.thumb = main.images.sizes[i].pop().source;
+				}
+			}else{
+			
+			}
+			if(column == 1){
+				column2.push({main:main, others:src, logo:source + ".png"});
+				column = 2;
+			}else{
+				column1.push({main:main, others:src, logo:source + ".png"});
+				column = 1;
+			}
+		}
+		res.render('index',{column1:column1, column2:column2});
+	})
+});
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
 });
+
+Array.prototype.clean = function(deleteValue) {
+  for (var i = 0; i < this.length; i++) {
+    if (this[i] == deleteValue) {         
+      this.splice(i, 1);
+      i--;
+    }
+  }
+  return this;
+};
+
+function getData(req,fn){
+	var cmd;
+	client.smembers('articles::sources', function(err, sources){
+		var build = {
+			content:{}
+		};
+		var sources_build = req.query.sources ? req.query.sources.split(',') : sources;
+		var limit = req.query.limit ? req.query.limit -1 : 5;
+		
+		//remove empty values
+		sources_build.clean("");
+		
+		async.forEach(sources_build, function(source, fn){
+			//remove sources which dont exist
+			if(sources.indexOf(source) === -1){
+				build.content[source] = [];
+				return fn(null);
+			};
+			
+			client.lrange('articles:' + source, 0, limit, function(err, data){
+				var parsed = [];
+				data.forEach(function(e){
+					var doc = JSON.parse(e);
+					parsed.push(doc);
+					console.log(doc.title);
+				})
+				build.content[source] = parsed;
+				return fn(null);
+			});
+		}, function(){
+			fn(build);
+		});
+	});
+}
